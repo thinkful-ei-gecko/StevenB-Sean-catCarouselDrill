25a5e79e0091a700662fd13fc8ebbc7cc3c32207
Steven Bull <stevenwbulljr@gmail.com>
2019-08-15 14:07:29 -0500

    finished cat carousel

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
+/* eslint globals $ */
+'use strict';
+
+function clickHandler() {
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(this).find('img').attr('src');
+    const altSrc = $(this).find('img').attr('alt');
+    $('.hero img').attr('src', imgSrc).attr('alt', altSrc);
   });
-  
+}
+
+$(clickHandler);
